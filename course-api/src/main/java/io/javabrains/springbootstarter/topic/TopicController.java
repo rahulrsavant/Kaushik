e64ff8e014666dc74cfee2ee3c9db16a7c1256dd
rahulrsavant <Vidula@9975>
2019-12-21 16:34:13 +0530

    Post Topic (Change in ArrayList it was Immutable)

--- course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,15 +18,20 @@ public class TopicController {
 	private TopicSerice topicService;
 	
 	@RequestMapping("/topics")
-	public List<Topic> getAllTopics() {
-		
-		return topicService.getAllTopics();
-				
+	//@RequestMapping(value="/topics",method=RequestMethod.GET)   <---Default Request mapping
+	public List<Topic> getAllTopics() {		
+		return topicService.getAllTopics();	
 	}
 	
 	@RequestMapping("/topics/{ID}")
 	public Topic getTopic(@PathVariable("ID") String id) {
 		return topicService.getTopic(id);
+		
+	}
+	
+	@RequestMapping(value="/topics",method=RequestMethod.POST)
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.postTopic(topic);
 		
 	}
 	
--- course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicSerice.java
+++ course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicSerice.java
@@ -1,5 +1,6 @@
 package io.javabrains.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,8 +9,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicSerice {
 
-	private List<Topic> topics = Arrays.asList(new Topic("1", "C#", "C# Description"),
-			new Topic("2", "C++", "C++ Description"));
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(new Topic("1", "C#", "C# Description"),
+			new Topic("2", "C++", "C++ Description")
+			));
 
 	public List<Topic> getAllTopics() {
 		return topics;
@@ -19,6 +21,11 @@ public class TopicSerice {
 		return topics.stream().filter(t -> t.getId().equals(id))
 				.findFirst().get();
 
+	}
+	
+	public void postTopic(Topic topic) {
+		topics.add(topic);
+		
 	}
 
 }
